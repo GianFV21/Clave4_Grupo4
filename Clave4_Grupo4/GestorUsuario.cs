5ef10569c4cdef9115aca4f08652e779aafe23fc
Dora Arlen Vaquerano Mata <VM21054@ti.ues.edu.sv>
2024-11-12 19:24:43 -0600

    Se agrego la conexion de la base de datos en el gestor de formulario

--- Clave4_Grupo4/GestorUsuario.cs
+++ Clave4_Grupo4/GestorUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +11,20 @@ using System.Windows.Forms;
 
 namespace Clave4_Grupo4
 {
+    internal class Conexion
+    {
+        //Codigo para conexion
+        static string servidor = "localhost"; // Nombre o id servidor SQL
+        static string bd = "cafetin"; // Nombre de la base
+        static string usuario = "root"; //Usuario de acceso a MySQL
+        static string password = "1234"; //Contraseña de usuario de acceso a MySQL
+                                         //Crearemos la cadena de conexión concatenando las variables
+        static string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor +
+        "; User Id=" + usuario + "; Password=" + password + "";
+        //Instancia para conexión a MySQL, recibe la cadena de conexión
+        static MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+    }
     public partial class GestorUsuario : Form
     {
         public GestorUsuario()
